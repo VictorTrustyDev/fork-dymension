44a00b8bc48d7ed46380ae7e5b1f6e18c4094f7b
VictorTrustyDev <me@victortrusty.dev>
2024-07-22 04:23:05 +0700

    on dev

--- x/dymns/keeper/proposal.go
+++ x/dymns/keeper/proposal.go
@@ -7,25 +7,36 @@ import (
 )
 
 func (k Keeper) MigrateChainIds(ctx sdk.Context, replacement []dymnstypes.MigrateChainId) error {
-	previousChainIds := make(map[string]dymnstypes.MigrateChainId)
+	previousChainIdsToNewChainId := make(map[string]string)
 
 	for _, r := range replacement {
-		previousChainIds[r.PreviousChainId] = r
+		previousChainIdsToNewChainId[r.PreviousChainId] = r.NewChainId
 	}
 
-	// migrate params
+	if err := k.migrateChainIdsInParams(ctx, previousChainIdsToNewChainId); err != nil {
+		return err
+	}
+
+	if err := k.migrateChainIdsInDymNames(ctx, previousChainIdsToNewChainId); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (k Keeper) migrateChainIdsInParams(ctx sdk.Context, previousChainIdsToNewChainId map[string]string) error {
 	params := k.GetParams(ctx)
 
 	if len(params.Chains.AliasesByChainId) > 0 {
 		newAliasesByChainId := make(map[string]dymnstypes.AliasesOfChainId)
 		for chainId, aliases := range params.Chains.AliasesByChainId {
-			if r, isPreviousChainId := previousChainIds[chainId]; isPreviousChainId {
-				if _, foundDeclared := params.Chains.AliasesByChainId[r.NewChainId]; foundDeclared {
+			if newChainId, isPreviousChainId := previousChainIdsToNewChainId[chainId]; isPreviousChainId {
+				if _, foundDeclared := params.Chains.AliasesByChainId[newChainId]; foundDeclared {
 					// we don't override, we keep the aliases of the new chain id
 
 					// ignore and remove the aliases of the previous chain id
 				} else {
-					newAliasesByChainId[r.NewChainId] = aliases
+					newAliasesByChainId[newChainId] = aliases
 				}
 			} else {
 				newAliasesByChainId[chainId] = aliases
@@ -38,8 +49,8 @@ func (k Keeper) MigrateChainIds(ctx sdk.Context, replacement []dymnstypes.Migrat
 		newCoinType60UniqueChainIds := make(map[string]bool)
 
 		for _, chainId := range params.Chains.CoinType60ChainIds {
-			if r, isPreviousChainId := previousChainIds[chainId]; isPreviousChainId {
-				newCoinType60UniqueChainIds[r.NewChainId] = true
+			if newChainId, isPreviousChainId := previousChainIdsToNewChainId[chainId]; isPreviousChainId {
+				newCoinType60UniqueChainIds[newChainId] = true
 			} else {
 				newCoinType60UniqueChainIds[chainId] = true
 			}
@@ -60,86 +71,97 @@ func (k Keeper) MigrateChainIds(ctx sdk.Context, replacement []dymnstypes.Migrat
 	}
 
 	if err := k.SetParams(ctx, params); err != nil {
-		k.Logger(ctx).Error("failed to update params", "error", err)
+		k.Logger(ctx).Error(
+			"failed to update params",
+			"error", err,
+			"migration-state", "aborted",
+		)
 		return err
 	}
 
-	// end of params migration
+	return nil
+}
 
-	// Migrate chain ids within configs of Dym-Name.
+func (k Keeper) migrateChainIdsInDymNames(ctx sdk.Context, previousChainIdsToNewChainId map[string]string) error {
 	// We only migrate for Dym-Names that not expired to reduce IO needed.
 
 	nonExpiredDymNames := k.GetAllNonExpiredDymNames(ctx, ctx.BlockTime().Unix())
-	if len(nonExpiredDymNames) > 0 {
-		for _, dymName := range nonExpiredDymNames {
-			newConfigs := make([]dymnstypes.DymNameConfig, len(dymName.Configs))
-			var anyConfigUpdated bool
-			for i, config := range dymName.Configs {
-				if config.ChainId != "" {
-					if r, isPreviousChainId := previousChainIds[config.ChainId]; isPreviousChainId {
-						config.ChainId = r.NewChainId
-						anyConfigUpdated = true
-					}
-				}
-
-				newConfigs[i] = config
-			}
-
-			if !anyConfigUpdated {
-				continue
-			}
-
-			dymName.Configs = newConfigs
-
-			if err := dymName.Validate(); err != nil {
-				k.Logger(ctx).Error(
-					"failed to migrate chain ids for Dym-Name",
-					"dymName", dymName.Name,
-					"error", err,
-				)
-				// Skip migration for this Dym-Name.
-				// We don't want to break the migration process for other Dym-Names.
-				// The replacement should be done later by the owner.
-				continue
-			}
-
-			// from here, any step can procedures dirty state, so we need to abort the migration
-
-			if err := k.BeforeDymNameConfigChanged(ctx, dymName.Name); err != nil {
-				k.Logger(ctx).Error(
-					"failed to migrate chain ids for Dym-Name",
-					"dymName", dymName.Name,
-					"step", "BeforeDymNameConfigChanged",
-					"error", err,
-				)
-				return err
-			}
-
-			if err := k.SetDymName(ctx, dymName); err != nil {
-				k.Logger(ctx).Error(
-					"failed to migrate chain ids for Dym-Name",
-					"dymName", dymName.Name,
-					"step", "SetDymName",
-					"error", err,
-				)
-				return err
-			}
-
-			if err := k.AfterDymNameConfigChanged(ctx, dymName.Name); err != nil {
-				k.Logger(ctx).Error(
-					"failed to migrate chain ids for Dym-Name",
-					"dymName", dymName.Name,
-					"step", "AfterDymNameConfigChanged",
-					"error", err,
-				)
-				return err
-			}
-
-			k.Logger(ctx).Info("migrated chain ids for Dym-Name", "dymName", dymName.Name)
-		}
+	if len(nonExpiredDymNames) < 1 {
+		return nil
 	}
 
-	// end of Dym-Name config migration
+	for _, dymName := range nonExpiredDymNames {
+		newConfigs := make([]dymnstypes.DymNameConfig, len(dymName.Configs))
+		var anyConfigUpdated bool
+		for i, config := range dymName.Configs {
+			if config.ChainId != "" {
+				if newChainId, isPreviousChainId := previousChainIdsToNewChainId[config.ChainId]; isPreviousChainId {
+					config.ChainId = newChainId
+					anyConfigUpdated = true
+				}
+			}
+
+			newConfigs[i] = config
+		}
+
+		if !anyConfigUpdated {
+			// Skip migration for this Dym-Name if nothing updated to reduce IO.
+			continue
+		}
+
+		dymName.Configs = newConfigs
+
+		if err := dymName.Validate(); err != nil {
+			k.Logger(ctx).Error(
+				"failed to migrate chain ids for Dym-Name",
+				"dymName", dymName.Name,
+				"step", "Validate",
+				"error", err,
+				"migration-state", "continue",
+			)
+			// Skip migration for this Dym-Name.
+			// We don't want to break the migration process for other Dym-Names.
+			// The replacement should be done later by the owner.
+			continue
+		}
+
+		// from here, any step can procedures dirty state, so we need to abort the migration
+
+		if err := k.BeforeDymNameConfigChanged(ctx, dymName.Name); err != nil {
+			k.Logger(ctx).Error(
+				"failed to migrate chain ids for Dym-Name",
+				"dymName", dymName.Name,
+				"step", "BeforeDymNameConfigChanged",
+				"error", err,
+				"migration-state", "aborted",
+			)
+			return err
+		}
+
+		if err := k.SetDymName(ctx, dymName); err != nil {
+			k.Logger(ctx).Error(
+				"failed to migrate chain ids for Dym-Name",
+				"dymName", dymName.Name,
+				"step", "SetDymName",
+				"error", err,
+				"migration-state", "aborted",
+			)
+			return err
+		}
+
+		if err := k.AfterDymNameConfigChanged(ctx, dymName.Name); err != nil {
+			k.Logger(ctx).Error(
+				"failed to migrate chain ids for Dym-Name",
+				"dymName", dymName.Name,
+				"step", "AfterDymNameConfigChanged",
+				"error", err,
+				"migration-state", "aborted",
+			)
+			return err
+		}
+
+		k.Logger(ctx).Info("migrated chain ids for Dym-Name", "dymName", dymName.Name)
+	}
 
 	return nil
 }
